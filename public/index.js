1e1efb42eb34f8da65751a82c92d990b14b290b1
Nicolas Baez <baeznicolas@itis-molinari.eu>
2025-02-15 17:50:08 +0100

    client upload

--- public/index.js
+++ public/index.js
@@ -7,3 +7,30 @@ homeBtn.onclick = () => {
     document.getElementById("home").style.display = 'block';
 }
 createLogin();
+
+(async () => {
+    const inputFile = document.querySelector('#file');
+    const button = document.querySelector("#button");
+    const link = document.querySelector("#link");
+  
+    handleSubmit = async (event) => {
+      const formData = new FormData();
+      formData.append("file", inputFile.files[0]);
+      const body = formData;
+      body.description = inputDescription.value;
+      const fetchOptions = {
+        method: 'post',
+        body: body
+      };
+      try {
+        const res = await fetch("/upload", fetchOptions);
+        const data = await res.json();
+        link.setAttribute("href", data.url);
+        link.innerText = data.url;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  
+    button.onclick = handleSubmit;
+  })();
